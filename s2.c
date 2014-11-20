8a01909c597e9043b1b885e04f698ac5c7c0c1bb
Mika Sarkin Jain <mikasarkinjain@gmail.com>
2014-11-20 13:46:21 -0500

    .

--- s2.c
+++ s2.c
@@ -45,6 +45,33 @@ char redirect(char* r){ //t: 1 - >, 2 - >>, 3 - 2>
   return 1;
 }
 
+char* runLine(char* l){
+  if (!strcmp(l, "exit")) exit(-1);
+  //if (!strstr(l, "cd")) //chdir(path);
+  
+  char* splitOnRedirect[1000] = {0};
+  splitOnRedirect[0] = 1;
+  char* p = l-1;
+  int i = 1;
+  while(p = strchr(p+1, '>')){
+      splitOnRedirect[i] = p+1;
+      *p = 0;
+      i++;
+  }
+  
+  i = 0;
+  while(splitOnRedirect[i]){
+    
+  }
+  
+  
+    
+    
+  if (!strchr(r, ">") && strchr(r, "|")) redirect(l[i]);
+  if (!strchr(r, ">") && !strchr(r, "|")) printf("%s", execute(l[i]));
+  //if (!strchr(r, ">") && strchr(r, "|")) redirect(l[i]);
+}
+
 main(){
   while(1){
     char r[1000];
@@ -57,18 +84,18 @@ main(){
     char* p = r-1;
     int i = 1;
     while(p = strchr(p+1, ';')){
-        c[i] = p+1;
+        l[i] = p+1;
         *p = 0;
         i++;
     }
     
-    int i;
+    i = 0;
     while(l[i])
-      if (!strcmp(l[i], "exit")) exit(-1);
-      if (!strcmp(l[i], "cd")) //chdir(path);
-      if (strchr(r, ">") && strchr(r, "|")) execute(l[i]);
-      if (!strchr(r, ">") && strchr(r, "|")) redirect(l[i]);
+      runLine(l[i]);
       i++;
     }
   }
 }
+
+
+
