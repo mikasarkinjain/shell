3d5c0db9e9e7f01d67f608adcb0347b822242665
Mika Sarkin Jain <mikasarkinjain@gmail.com>
2014-11-20 14:05:55 -0500

    .

--- s3.c
+++ s3.c
@@ -1,1 +1,34 @@
- 
+main(){
+  while(1){
+    char r[1000];
+    printf("> ");
+    fgets(r, sizeof r, stdin);
+    *strchr(r, '\n') = 0;
+    
+    char* l[1000] = {0};
+    l[0] = r;
+    char* p = r-1;
+    int i = 1;
+    while(p = strchr(p+1, ';')){
+        c[i] = p+1;
+        *p = 0;
+        i++;
+    }
+    
+    i = 0;
+    while(l[i]){
+     char p = l[i];
+     char currentOutput[1000];
+     char firstPipe = 1;
+     while(strchr(p, "|")){
+      if (firstPipe){
+       currentOutput = execute(SUBSTRING(p, strchr(p, "|")-1));
+       firstPipe = 1;
+      }
+      else {
+       pipe(currentOutput, _______________)
+      }
+      p = strchr(p, ">");
+    }
+  }
+}
