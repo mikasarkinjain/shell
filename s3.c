f8aaabca86edf7c3314feef530d1d73078bfdd05
Mika Sarkin Jain <mikasarkinjain@gmail.com>
2014-11-20 23:02:06 -0500

    parser done

--- s3.c
+++ s3.c
@@ -10,19 +10,19 @@ main(){
     char* p = r-1;
     int i = 1;
     while(p = strchr(p+1, ';')){
-        c[i] = p+1;
+        l[i] = p+1;
         *p = 0;
         i++;
     }
     
     i = 0;
     while(l[i]){
-     char p = l[i];
+     char* p = l+i;
      char currentOutput[1000] = {0};
      currentOuput = execute(SUBSTRING(p, strchr(p, "|")));
      while(strchr(p, "|")){
       currentOutput = pipe(currentOutput, SUBSTRING(strchr(p, "|")+1, strchr(strchr(p, "|"), "|")));
-      p = strchr(p, "|");
+      p = strchr(p, "|")+1;
       }
     printf("%s\n", currentOutput);
     }
@@ -63,11 +63,11 @@ main(){
      while(strchr(p, "|")){
       if (strchr(p, "|") && !strchr(p, ">") || strchr(p, "|") && strchr(p, ">") && strchr(p, "|") < strchr(p, ">")){
         currentOutput = pipe(currentOutput, SUBSTRING(strchr(p, "|")+1, strchr(strchr(p, "|"), "|")));
-        p = strchr(p, "|");
+        p = strchr(p, "|")+1;
       } 
       if (strchr(p, ">") && !strchr(p, "|") || strchr(p, ">") && strchr(p, "|") && strchr(p, ">") < strchr(p, "|")){
         currentOutput = redirect(currentOutput, SUBSTRING(strchr(p, "|")+1, strchr(strchr(p, "|"), "|")));
-        p = strchr(p, ">");
+        p = strchr(p, ">")+1;
       }
       }
     printf("%s\n", currentOutput);
