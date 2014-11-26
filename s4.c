53157cd09bbb9fe2a1cbb4243605a10187453f1f
ezchen <echen2015@gmail.com>
2014-11-26 14:06:35 -0500

    doesnt work right now

--- s4.c
+++ s4.c
@@ -1,6 +1,7 @@
-/* 
+/*
  * todo:
- * stop leakage from redirection (close files)
+ * wait for child process
+ * handle extra spaces for exec ("ls " breaks)
  * extend execute so it doesn't rely on space separation
  * actually get program running
  * print specific errors when they occur
@@ -9,6 +10,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 /* counts number of tokens */
 int count_tokens(char *s, char delim){
@@ -29,19 +32,22 @@ char **split(char *s, char delim){
 }
 
 char execute(char* r, int in, int out){
-	f = fork();
-	if (!f){
+	int f = fork();
+	if (f == 0){
+                printf("in: %d\nout: %d\n", in, out);
 		dup2(in, STDIN_FILENO);
 		dup2(out, STDOUT_FILENO);
+                close(out);
+                printf("after dup2\n");
 		int n = count_tokens(r, ' '), lend = 0;
 		char **c = split(r, ' ');
-		char *d[1000];
+		char *reduced[1000];
 		int i=0, j=0, fd;
 		while(i<n){
 			if(!strcmp(">", c[i])){
 				fd = open(c[i+1], O_CREAT | O_WRONLY | O_TRUNC, 0777);
 				dup2(fd, STDOUT_FILENO);
-				
+
 			}
 			else if(!strcmp(">>", c[i])){
 				fd = open(c[i+1], O_CREAT | O_WRONLY | O_APPEND, 0777);
@@ -65,46 +71,21 @@ char execute(char* r, int in, int out){
 				dup2(fd, STDIN_FILENO);
 			}
 			else {
-				d[i] = c[i];
+				reduced[i] = c[i];
 				lend++;
 				i--;
 			}
 			i += 2;
 		}
 		if(lend){
-			execvp(d[0], d);
-			printf("Unknown Command");
+                        printf("Above exec\n");
+			execvp(reduced[0], reduced);
+			printf("Unknown Command\n");
 			return 1;
 		}
 	}
 }
 
-/*
-   char redirect(char* r){ //t: 1 - >, 2 - >>, 3 - 2>
-   f = fork;
-   if (!f){
-   if (&(strchr(r, ">")+1)==">"){
-//int fd = open(f, O_WRONLY | O_APPEND, 0777);
-//close(fd);
-//dup2(fd,STDOUT_FILENO);
-//execlp(e, e, NULL);
-}
-
-else if ((strchr(r, ">") != r && &(strchr(r, ">")-1)=="2"){
-//int fd = open(f, O_WRONLY | O_APPEND, 0777);
-//close(fd);
-//dup2(fd,STDERR_FILENO);
-//execlp(e, e, NULL);
-}
-
-//int fd = open(f, O_WRONLY, 0777);
-//close(fd);
-//dup2(fd,STDOUT_FILENO);
-//execlp(e, e, NULL);
-}
-return 1;
-}
-*/
 main(){
 	int i, j, npipes;
 	int fdin, fdout;
@@ -119,7 +100,7 @@ main(){
 		while (l[i]) {
 			// "exit" case
 			// "cd" case
-			npipes = count_tokens(l[i], '|');
+			npipes = count_tokens(l[i], '|') - 1;
 			lpipe = split(l[i], '|');
 			j = 0;
 			while (lpipe[j]) {
@@ -136,30 +117,13 @@ main(){
 				if(j == npipes)
 					fdout = 1;
 				execute(lpipe[j], fdin, fdout);
+                                if (j != 0)
+                                    close(fdin);
+                                if (j != npipes)
+                                    close(fdout);
 				j++;
 			}
 			i++;
 		}
 	}
 }
-
-		/*    char* l[1000] = {0};
-		      l[0] = r;
-		      char* p = r-1;
-		      int i = 1;
-		      while(p = strchr(p+1, ';')){
-		      l[i] = p+1;
-		 *p = 0;
-		 i++;
-		 }
-
-		 i = 0;
-		 while(l[i])
-		 char *p = l+i;
-		 if (!strcmp(l[i], "exit")) exit(-1);
-		 if (!strcmp(l[i], "cd")) //chdir(path);
-		 if (strchr(r, ">") && strchr(r, "|")) execute(l[i]);
-		 if (!strchr(r, ">") && strchr(r, "|")) redirect(l[i]);
-		 i++;
-		 }
-		 */
